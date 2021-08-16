c9f5c0ffd2e5b6b661152a8c50678f25159678b8
govardhan2598 <75354148+govardhan2598@users.noreply.github.com>
2021-08-16 13:45:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:blue;">HELLO IDEYALABS</h1> \n');
+  res.send('<h1 style="color:blue;">Hello IdeyaLabs Devops Team</h1> \n');
 });
 
 app.listen(PORT, HOST);
