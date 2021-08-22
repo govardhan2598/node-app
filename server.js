e3855bb2e682b4f482cf61860ac72cdf3c0f9e64
govardhan2598 <75354148+govardhan2598@users.noreply.github.com>
2021-08-22 21:15:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,11 +9,15 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:lime;">Hello IdeyaLabs Devops Team, Hyderabad</h1> \n');
+  res.send('<h1 style="color:lime;">Hello IdeyaLabs Devops Team </h1> \n');
 });
 
 app.get('/Team', (req, res) => {
   res.send('<h1 style="color:aqua;">Govardhan A  Rajeshwarayya S  Aravind D  Gopi T  Yaswanth Y  SaiTeja A </h1> \n');
+});
+
+app.get('/location', (req, res) => {
+  res.send('<h1 style="color:maroon;">Hyderabad</h1> \n');
 });
 
 app.listen(PORT, HOST);
